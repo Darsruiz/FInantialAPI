cb158c6247e132a8296e060f2c6265c6075a467f
Darsruiz <darsruiz@gmail.com>
2024-06-12 21:09:51 +0200

    added methods to Interface

--- FInantialAPI/Interfaces/IAccountService.cs
+++ FInantialAPI/Interfaces/IAccountService.cs
@@ -1,9 +1,17 @@
 ﻿using FInantialAPI.Models;
+using FInantialAPI.Models.Infrastructure;
 
 namespace FInantialAPI.Interfaces
 {
     public interface IAccountService
     {
-        List<Transaction> GetMovements(int accountId);
+        List<TransactionModel> GetMovements(int accountId);
+        bool Withdraw(int cardId, WithdrawalRequestModel request);
+        bool Deposit(int cardId, DepositRequestModel request);
+        bool Transfer(int accountId, TransferRequestModel request);
+        bool ActivateCard(int accountId, int cardId);
+        bool ChangeCardPIN(int accountId, int cardId, PinChangeRequestModel request);
+
+
     }
 }
